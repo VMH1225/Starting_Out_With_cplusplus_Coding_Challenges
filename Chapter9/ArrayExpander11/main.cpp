99336fa19ce08b1db3efefa4f2390d37b67e7ad4
VMH1225 <huertavictor1225@gmail.com>
2022-11-06 15:06:25 -0800

    initial commit

---
+++ Chapter9/ArrayExpander11/main.cpp
@@ -0,0 +1,14 @@
+/* Chapter 9 Pointers 
+* 11) Array Expander
+* Write a function that acccepts an int array and the array's size as arguments. The function should create a new array that is
+* twice the size of the argument array. The function should copy the contents of the argument array to the new array and initialize
+* the unused elements of the second array with 0. The function should return a pointer to the new array.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+}
