3aad8ee42a1f1d538d9aa4fd058f87628dd83942
VMH1225 <huertavictor1225@gmail.com>
2022-11-08 22:53:00 -0800

    added function to double size of array

--- Chapter9/ArrayExpander11/main.cpp
+++ Chapter9/ArrayExpander11/main.cpp
@@ -1,4 +1,4 @@
-/* Chapter 9 Pointers 
+/* Chapter 9 Pointers  page 555
 * 11) Array Expander
 * Write a function that acccepts an int array and the array's size as arguments. The function should create a new array that is
 * twice the size of the argument array. The function should copy the contents of the argument array to the new array and initialize
@@ -7,8 +7,41 @@
 
 #include <iostream>
 
+int* copyValuesDoubleSizeOfArray(int arr[], int size);
 using namespace std;
 
-int main() {
+int main() {	
+	const int SIZE = 10;
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++) {
+		arr[i] = i;
+	}
 	
+	for (int val : arr) {
+		cout << val << endl;
+	}
+	int* ptr1 = nullptr;
+
+	ptr1 = copyValuesDoubleSizeOfArray(arr, SIZE);
+
+	for (int i = 0; i < SIZE * 2; i++) {
+		cout << ptr1[i] << endl;
+	}
+}
+
+
+int* copyValuesDoubleSizeOfArray(int arr[], int size) {
+	int* ptr = nullptr;
+	const int DOUBLE_SIZE = size * 2;
+	ptr = new int[DOUBLE_SIZE];
+
+	for (int i = 0; i < DOUBLE_SIZE; i++) {
+		if (i < size) {
+			ptr[i] = arr[i];
+		}
+		else {
+			ptr[i] = 0;
+		}
+	}
+	return ptr;
 }
